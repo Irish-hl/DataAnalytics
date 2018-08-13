22e5b65178b8decd50c3631f67282016b8cd3cbf
Irish-hl <irishuangling@163.com>
2018-08-14 00:36:35 +0800

    Front end static page

--- DataAnalytics/Controllers/HomeController.cs
+++ DataAnalytics/Controllers/HomeController.cs
@@ -14,5 +14,29 @@ namespace DataAnalytics.Controllers
 
             return View();
         }
+        public ActionResult Chart()
+        {
+            ViewBag.Title = "Chart Page";
+
+            return View();
+        }
+        public ActionResult Signin()
+        {
+            ViewBag.Title = "Signin Page";
+
+            return View();
+        }
+        public ActionResult Signup()
+        {
+            ViewBag.Title = "Signup Page";
+
+            return View();
+        }
+        public ActionResult Portfolio()
+        {
+            ViewBag.Title = "Portfolio Page";
+
+            return View();
+        }
     }
 }
